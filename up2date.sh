a121397506631a0f9f941b50341437bbbd9dfde4
The Sang Nguyen <77398269+thesang-nguyen@users.noreply.github.com>
2024-05-24 14:59:00 +0200

    added echo commans for better readability

--- up2date.sh
+++ up2date.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+
+echo "== 1 == rm -rf /var/lib/dpkg/lock-frontend"
 rm -rf /var/lib/dpkg/lock-frontend
+
+echo "== 2== rm -rf /var/lib/dpkg/lock"
 rm -rf /var/lib/dpkg/lock
+
+echo "== 3 == apt-get update"
 apt-get update
+
+echo "== 4 == apt-get upgrade -y"
 apt-get upgrade -y
+
+echo "== 5 == apt-get dist-upgrade -y"
 apt-get dist-upgrade -y
+
+echo "== 6 == apt-get autoremove -y"
 apt-get autoremove -y
+
+echo "== 7 == apt-get autoclean -y"
 apt-get autoclean -y
